673ada53e9584b89248cb45d5d1e19fd95720b18
Jung Yeon Park <jpark0@gmail.com>
2019-04-30 23:26:17 +0900

    Create Flowline
    - Finish initialize
    TODO: flowline.run

--- pycptmodels/flowline.py
+++ pycptmodels/flowline.py
@@ -1,20 +1,30 @@
 import numpy as np
 import random
 
-class Flowline:
+from input import PoissonProcessInput
+
+class ParametricFlowline:
   def __init__(self, 
-              flow=
-              [0, 1, 1, 1, 1, 2, 2, 3, 4, 3, 3, 3, 3, 2, 2, 0],
-              R=
+              flow=[
+              [1, 1, 1, 1, 1, 2, 2, 3, 4, 3, 3, 3, 3, 2, 2, 1],
+              [1, 1, 1, 1, 1, 1, 2, 2, 4, 3, 3, 3, 3, 2, 2, 1],
+              [1, 1, 1, 1, 2, 2, 2, 1, 2, 3, 4, 3, 3, 3, 3, 2, 2, 1],],
+              R=[
               [1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 3, 2, 2, 1],
-              PT=
+              [1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 3, 2, 2, 1],
+              [1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 3, 2, 2, 1],],
+              PT=[
               [0, 80, 90, 60, 65, 50, 90, 60, 100, 90, 60, 90, 130, 90, 60, 0],
-              buffer_R=[1, 1, 16]):
+              [0, 80, 90, 60, 65, 90, 60, 50, 100, 90, 60, 90, 130, 90, 60, 0],
+              [0, 80, 90, 60, 50, 90, 60, 65, 90, 60, 100, 90, 60, 90, 130, 90, 60, 0],],
+              buffer_R=[4, 8, 16],
+              move=3, 
+              pick=1):
     ''' Create Flowline model
     
     :param flow: Process flows. Must be a list for each lot class, where each list contains cluster indices
     Positive integer for each cluster with a robot arm, starting from left to right.
-    0 for indexer
+    1 for indexer
     :type flow: list of list
 
     :param R: Redundancies of process flow. Must be a list for each lot class
@@ -25,51 +35,98 @@ class Flowline:
 
     :param buffer_R: Buffer sizes, starting from left to right. Length of list must be equal to max cluster index - 1.
     :type buffer_R: list of int
+
+    :param move: Robot move time
+    :type move: int
+
+    :param pick: Robot pick time
+    :type pick: int
     '''
+    # K = Number of lot classes
+    self.K = len(flow)
+    
     self.flow = flow
     self.R = R
     self.PT = PT
     self.buffer_R = buffer_R
+    self.move = move
+    self.pick = pick
+
   
-  def Initialize(self):
+  def initialize(self):
+    ''' Create new process flows including buffers for parametric flow line. Modify process times
+    '''
     
     # Add buffers to flow, R, and PT
-    m = 0
-    for input_m in range(len(self.flow) - 1):
-      # Check if m < m+1 (Prescan, different clusters)
-      if self.flow[m] < self.flow[m+1]:
-        # Check that m is not INDEXER and m+1 != 1
-        if not (self.flow[m] == 0 and self.flow[m+1] == 1):
-          # Prescan inlet buffer index = -1
-          self.flow.insert(m+1, -1)
+    for k in range(self.K):
+
+      m = 0
+      orig_len = len(self.flow[k])
+      for input_m in range(orig_len - 1):
+        # Check if m < m+1, different clusters prescan case
+        # Add as many buffers as the difference in clusters
+        for c in range(self.flow[k][m], self.flow[k][m+1]):
+          # Buffer flow = -1
+          self.flow[k].insert(m+1, -1)
 
           # Get buffer size
-          b = self.buffer_R[self.flow[m] - 1]
-          self.R.insert(m+1, max(b - 1, 1))
+          b = self.buffer_R[c - 1]
+          self.R[k].insert(m+1, max(b - 1, 1))
 
-          self.PT.insert(m+1, 0)
-          
+          self.PT[k].insert(m+1, 0)
+
           # Increment to skip just added buffer
           m = m + 1
-      
-      # Check if m > m+1 (Post-scan, different clusters)
-      if self.flow[m] > self.flow[m+1]:
-        # Check that m != 1 and m+1 is not INDEXER
-        if not (self.flow[m] == 1 and self.flow[m+1] == 0):
-          # Post-scan outlet buffer index = -2
-          self.flow.insert(m+1, -2)
 
-          # Outlet buffer R = 1
-          self.R.insert(m+1, 1)
-
-          self.PT.insert(m+1, 0)
+        # Check if m > m+1, different clusters postscan case
+        # Add as many buffers as the difference in clusters
+        for c in range(self.flow[k][m], self.flow[k][m+1], -1):
+          # Buffer flow = -1
+          self.flow[k].insert(m+1, -1)
           
+          # Outlet buffer size = 1
+          self.R[k].insert(m+1, 1)
+
+          self.PT[k].insert(m+1, 0)
+           
           # Increment to skip just added buffer
           m = m + 1
-      m = m + 1
 
-    # Modify PT
-    
+        m = m + 1
 
-FL = Flowline()
-FL.Initialize()
+    # Add dummy modules to match maximum flow length
+    max_len = max(len(f) for f in self.flow)
+    self.dummy = [0] * self.K
+    for k in range(self.K):
+      self.dummy[k] = max_len - len(self.flow[k])
+      if self.dummy[k] > 0:
+        # Use -2 for dummy modules in flow
+        self.flow[k] = ([-2] * self.dummy[k]) + self.flow[k]
+        self.R[k] = ([1] * self.dummy[k]) + self.R[k]
+        self.PT[k] = ([0] * self.dummy[k]) + self.PT[k]
+
+    # Modify process times
+    # Argmax to get bottleneck and penultimate bottleneck modules
+    L = np.argsort(-np.divide(self.PT, self.R), axis=1)
+    self.BN = L[:, 0].tolist()
+    self.PBN = L[:, 1].tolist()
+    PT_to_add = np.zeros_like(self.PT[0])
+    for k in range(self.K):
+      PT_to_add[self.dummy[k]:-1] = self.move + 2 * self.pick
+      PT_to_add[self.BN[k]] = 2 * self.move + 4 * self.pick
+      PT_to_add[self.PBN[k]] = 3 * self.move + 4 * self.pick
+      self.PT[k] = np.add(self.PT[k], PT_to_add).tolist()
+
+  def run(self, input):
+    maxR = max([self.R[k][0] for k in range(self.K)])
+
+
+    print("here")
+
+FL = ParametricFlowline()
+FL.initialize()
+
+input = PoissonProcessInput(N=10, lambda_=2000, lotsizes=[23,24,25], lotsize_weights=[0.25,0.5,0.25], reticle=[210,260], prescan=[0,0], K=3)
+input.initialize()
+
+FL.run(input)
--- pycptmodels/input.py
+++ pycptmodels/input.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random
 
-class Input:
+class PoissonProcessInput:
   def __init__(self, N, lambda_, lotsizes, lotsize_weights, reticle, prescan, K):
     ''' Create Input object
     
@@ -35,7 +35,7 @@ class Input:
     self.prescan_params = prescan
     self.K = K
   
-  def sample_arrivals(self):
+  def _sample_arrivals(self):
     ''' Poisson Process lot arrival times
     '''
     X_t = np.random.exponential(self.lambda_, self.N-1)
@@ -43,35 +43,29 @@ class Input:
     arrivals = np.insert(arrivals, 0, 0.)
     self.A = arrivals.tolist()
 
-  def sample_lotsizes(self):
+  def _sample_lotsizes(self):
     ''' Generate lot sizes according to probabilities
     '''
     self.W = random.choices(self.lotsizes, self.lotsize_weights, k = self.N)
 
-  def sample_reticle(self):
+  def _sample_reticle(self):
     ''' Generate reticle alignment setup times from uniform distribution
     '''
     self.tau_R = np.random.uniform(*self.reticle_params, size=self.N).tolist()
 
-  def sample_prescan(self):
+  def _sample_prescan(self):
     ''' Generate prescan setup times from uniform distribution
     '''
     self.tau_S = np.random.uniform(*self.prescan_params, size=self.N).tolist()
 
-  def sample_lotclass(self):
+  def _sample_lotclass(self):
     ''' Generate random lot classes
     '''
     self.lotclass = np.random.randint(1, self.K + 1, size=self.N).tolist()
 
-  def Initialize(self):
-    self.sample_arrivals()
-    self.sample_lotsizes()
-    self.sample_lotclass()
-    self.sample_reticle()
-    self.sample_prescan()
-
-
-input = Input(N=10, lambda_=2000, lotsizes=[23,24,25], lotsize_weights=[0.25,0.5,0.25], reticle=[210,260], prescan=[0,0], K=3)
-input.Initialize()
-
-print("here")
+  def initialize(self):
+    self._sample_arrivals()
+    self._sample_lotsizes()
+    self._sample_lotclass()
+    self._sample_reticle()
+    self._sample_prescan()
