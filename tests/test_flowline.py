f18472443cf0a212f1de47b35c594807da14e42a
Jung Yeon Park <jpark0@gmail.com>
2019-05-02 22:46:11 +0900

    Add no prescan setup test for flowline

--- tests/test_flowline.py
+++ tests/test_flowline.py
@@ -41,6 +41,7 @@ def test_initialize():
     assert np.array_equal(FL.dummy, [4, 4, 0])
     
 def test_run():
+    ## Prescan test
     input = PoissonProcessInput(N=5, lambda_=0, lotsizes=[5], lotsize_weights=[
                                 1], reticle=[250, 250], prescan=[400, 400], K=3)
     input.initialize()
@@ -80,4 +81,42 @@ def test_run():
     # Completion times
     assert np.array_equal(FL.C, [2323., 3123., 3923., 5213., 6418.])
 
+    ## No Prescan test
+    input = PoissonProcessInput(N=5, lambda_=0, lotsizes=[5], lotsize_weights=[
+                                1], reticle=[250, 250], prescan=[0, 0], K=3)
+    input.initialize()
 
+    # Fix arrival times and lot classes
+    input.lotclass = [1, 1, 0, 2, 0]
+    input.A = [0, 100, 150, 500, 600]
+
+    FL = ParametricFlowline(
+      flow=[
+        [1, 1, 1, 1, 1, 2, 2, 3, 4, 3, 3, 3, 3, 2, 2, 1],
+        [1, 1, 1, 1, 1, 1, 2, 2, 4, 3, 3, 3, 3, 2, 2, 1],
+        [1, 1, 1, 1, 2, 2, 2, 1, 2, 3, 4, 3, 3, 3, 3, 2, 2, 1], ],
+      R=[
+        [1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 3, 2, 2, 1],
+        [1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 3, 2, 2, 1],
+        [1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 3, 2, 2, 1], ],
+      PT=[
+        [0, 80, 90, 60, 65, 50, 90, 60, 100, 90, 60, 90, 130, 90, 60, 0],
+        [0, 80, 90, 60, 65, 90, 60, 50, 100, 90, 60, 90, 130, 90, 60, 0],
+        [0, 80, 90, 60, 50, 90, 60, 65, 90, 60, 100, 90, 60, 90, 130, 90, 60, 0], ],
+      buffer_R=[1, 1, 16],
+      move=3,
+      pick=1
+    )    
+    FL.initialize()
+    FL.run(input)
+
+    # Check shape of FL.X
+    assert np.array(FL.X).shape == (25, 26)
+
+    # Check last_prescan
+    assert np.array_equal(FL.last_prescan, [13, 12, 13])
+
+    # Start times
+    assert np.array_equal(FL.S, [0., 185., 380., 500., 1115.])
+    # Completion times
+    assert np.array_equal(FL.C, [1923., 2723., 3523., 4323., 5123.])
